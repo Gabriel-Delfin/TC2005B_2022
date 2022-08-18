9c69240bba3e38b5b247c3d895997bb8cf9ba5c1
Gabriel <a01701482@tec.mx>
2022-08-18 12:42:51 -0500

    Crear y ejecutar el servidor http

--- Lab 7/app.js
+++ Lab 7/app.js
@@ -1,5 +1,30 @@
+setTimeout(() => console.log("código asíncrono"), 4000);
+setTimeout(() => console.error("ERROR: System Failure"), 7000);
+
 const filesystem = require('fs');
 filesystem.writeFileSync('hola.txt', 'Hola desde node');
-filesystem.writeFileSync('hola.txt', 'Saludos');
 
 console.log("hola desde node");
+
+
+const arreglo = [5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000,340, 1000, 50];
+
+//Imprime el arreglo en orden porque se ejecuta con los timeouts de los valores del arreglo
+for (let item of arreglo) {
+    setTimeout(() => {
+        console.log(item);
+    }, item);
+    //setTimeout detona el valor en cuanto a milisegundos
+}
+
+
+const http = require('http');
+
+const server = http.createServer( (request, response) => {
+    console.log(request.url);
+    response.setHeader('Content-Type', 'text/html');
+    response.write("");
+    response.end();
+});
+
+server.listen(3000);
