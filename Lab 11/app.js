9ca111c0a97eb45a85afc716b7e8629e5b424197
Gabriel <a01701482@tec.mx>
2022-08-25 03:48:04 -0500

    Laboratorio 5

--- Lab 11/app.js
+++ Lab 11/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 //Middleware
 app.use((request, response, next) => {
@@ -8,8 +12,28 @@ app.use((request, response, next) => {
 });
 
 app.use((request, response, next) => {
+    console.log('Segundo Middleware');
+    next();
+});
+
+//Se tiene que ir desde lo más específico hacia lo general
+//Solo hay una respuesta
+app.post('/trivia', (request, response, next) => {
+    console.log(request.body);
+    next();
+});
+
+app.use('/hola/lalo', (request, response, next) => {
+    response.send('Hola! de la ruta "/hola/lalo"'); 
+});
+
+app.use('/hola', (request, response, next) => {
+    response.send('Hola! de la ruta "/hola"'); 
+});
+
+app.use((request, response, next) => {
     console.log('Otro middleware sin reiniciar!');
     response.send('¡Hola mundo!'); //Manda la respuesta
-});
+})
 
 app.listen(3000);
